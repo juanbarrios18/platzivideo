4117477d9034794cda9a59444eaa28ab657c5e24
JUANFRAN <JUANFRAN@esl-c02w95g8hv29.home>
2020-04-16 12:20:05 +0200

    Curso de react route done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../assets/styles/components/Header.scss'
 import logo from '../assets/static/logo-platzi-video-BW2.png'
 import user from '../assets/static/user-icon.png'
 
 const Header = () => (
   <header className='header'>
-    <img src={logo} className='header__img' alt='header__img' />
+    <Link to='/'>
+      <img src={logo} className='header__img' alt='header__img' />
+    </Link>
     <div className='header__menu'>
       <div className='header__menu_profile'>
         <img src={user} alt='user-icon' />
@@ -13,7 +16,9 @@ const Header = () => (
       </div>
       <ul>
         <li><a href='/'>Cuenta a</a></li>
-        <li><a href='/'>Cerrar Sesión</a></li>
+        <Link to='/login'>
+        Iniciar sesión
+        </Link>
       </ul>
     </div>
   </header>
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
-import Header from '../components/header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
-import Footer from '../components/Footer'
 import useInitialState from '../hooks/useInitialState'
 import '../assets/styles/app.scss'
 
 const API = 'http://localhost:3000/initialState/'
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API)
 
   return initialState.length === 0 ? <h1>Loading...</h1> : (
-    <div className='app'>
-      <Header />
+    <>
       <Search />
 
       {initialState.mylist.length > 0 &&
@@ -42,9 +39,8 @@ const App = () => {
           )}
         </Carousel>
       </Categories>
-      <Footer />
-    </div>
+    </>
   )
 }
 
-export default App
+export default Home
