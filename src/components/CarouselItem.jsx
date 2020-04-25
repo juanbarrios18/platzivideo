75738904eb90d9101111f6e293e2ab23739b4562
JUANFRAN <JUANFRAN@esl-c02w95g8hv29.home>
2020-04-25 19:56:50 +0200

    react router / redux DONE

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,24 +1,49 @@
 import React from 'react'
-import '../assets/styles/components/CarouselItem.scss'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { setFavorite, deleteFavorite } from '../actions'
 import play from '../assets/static/play-icon.png'
 import plus from '../assets/static/plus-icon.png'
+import remove from '../assets/static/remove-icon.png'
 import PropTypes from 'prop-types'
+import '../assets/styles/components/CarouselItem.scss'
 
-const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
-  <div className='carousel-item'>
-    <img className='carousel-item__img' src={cover} alt={title} />
-    <div className='carousel-item__details'>
-      <div>
-        <img className='carousel-item__details--img' src={play} alt='Play Icon' />
-        <img className='carousel-item__details--img' src={plus} alt='Plus Icon' />
+const CarouselItem = (props) => {
+  const { id, cover, title, year, contentRating, duration, isList } = props
+
+  const handleSetFavorite = () => {
+    props.setFavorite(
+      {
+        id, cover, title, year, contentRating, duration
+      }
+    )
+  }
+
+  const handleDeleteFavorite = (itemId) => {
+    props.deleteFavorite(itemId)
+  }
+
+  return (
+    <div className='carousel-item'>
+      <img className='carousel-item__img' src={cover} alt={title} />
+      <div className='carousel-item__details'>
+        <div>
+          <Link to={`/player/${id}`}>
+            <img className='carousel-item__details--img' src={play} alt='Play Icon' />
+          </Link>
+          {isList
+            ? <img className='carousel-item__details--img' src={remove} alt='Remove Icon' onClick={() => handleDeleteFavorite(id)} />
+            : <img className='carousel-item__details--img' src={plus} alt='Plus Icon' onClick={handleSetFavorite} />}
+        </div>
+        <p className='carousel-item__details--title'>{title}</p>
+        <p className='carousel-item__details--subtitle'>
+          {`${year} ${contentRating} ${duration}`}
+        </p>
       </div>
-      <p className='carousel-item__details--title'>{title}</p>
-      <p className='carousel-item__details--subtitle'>
-        {`${year} ${contentRating} ${duration}`}
-      </p>
     </div>
-  </div>
-)
+
+  )
+}
 
 CarouselItem.propTypes = {
   cover: PropTypes.string,
@@ -28,4 +53,9 @@ CarouselItem.propTypes = {
   duration: PropTypes.number
 }
 
-export default CarouselItem
+const mapDispatchToProps = {
+  setFavorite,
+  deleteFavorite
+}
+
+export default connect(null, mapDispatchToProps)(CarouselItem)
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import gravatar from '../utils/gravatar'
+import {logoutRequest } from '../actions'
 import '../assets/styles/components/Header.scss'
 import logo from '../assets/static/logo-platzi-video-BW2.png'
-import user from '../assets/static/user-icon.png'
+import userIcon from '../assets/static/user-icon.png'
 
-const Header = () => (
-  <header className='header'>
-    <Link to='/'>
-      <img src={logo} className='header__img' alt='header__img' />
-    </Link>
-    <div className='header__menu'>
-      <div className='header__menu_profile'>
-        <img src={user} alt='user-icon' />
-        <p>perfil</p>
+const Header = props => {
+  const { user } = props
+  const hasUser = Object.keys(user).length > 0
+
+  const handleLogout = () => {
+    props.logoutRequest({})
+  }
+  return (
+    <header className='header'>
+      <Link to='/'>
+        <img src={logo} className='header__img' alt='header__img' />
+      </Link>
+      <div className='header__menu'>
+
+        <div className='header__menu_profile'>
+          {hasUser
+            ? <img src={gravatar(user.email)} alt={user.email} />
+            : <img src={userIcon} alt='user-icon' />}
+          <p>perfil</p>
+        </div>
+
+        <ul>
+          {hasUser
+            ? <li><a href='/'>{user.name}</a></li>
+            : null}
+          {hasUser
+            ? <li><a href='#logout' onClick={handleLogout}>Cerrar Sesión</a></li>
+            : <Link to='/login'>Iniciar sesión</Link>}
+        </ul>
+
       </div>
-      <ul>
-        <li><a href='/'>Cuenta a</a></li>
-        <Link to='/login'>
-        Iniciar sesión
-        </Link>
-      </ul>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
 
-export default Header
+const mapDispatchToProps = {
+  logoutRequest
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
